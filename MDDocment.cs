8ed270a4290ca08af59bbca6767ca438953b3884
LMing123 <z971130192@live.com>
2020-04-15 19:39:12 +0800

    initial

---
+++ MDDocment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace MDTools
+{
+    public class MDDocment
+    {
+        private StringBuilder _doc;
+        public MDDocment()
+        {
+            _doc = new StringBuilder();
+        }
+        public void AppendTitle1(string content) => AppendTitle(1, content);
+        public void AppendTitle2(string content) => AppendTitle(2, content);
+        public void AppendTitle3(string content) => AppendTitle(3, content);
+        public void AppendTitle4(string content) => AppendTitle(4, content);
+        public void AppendTitle5(string content) => AppendTitle(5, content);
+        public void AppendTitle6(string content) => AppendTitle(6, content);
+
+        public void AppendContent(string content) => _doc.Append(content);
+        public void AppendNewLine()
+        {
+            _doc.Append(" " + Environment.NewLine);
+        }
+        public void AppendNewLine(string content)
+        {
+            _doc.Append(content);
+            _doc.Append(" " + Environment.NewLine);
+        }
+        public void AppendLink(string linkName, string url) => _doc.Append($"[{linkName}]({url}) ");
+        public void AppendOnlineImg(string imgName, string url)
+        {
+            _doc.Append($"![{imgName}]({url}) ");
+        }
+        public void AppendTable(MDTable table)
+        {
+            _doc.Append(table.ToString());
+        }
+        public string Save() => _doc.ToString();
+
+        private void AppendTitle(int level, string content)
+        {
+            if (level > 6) level = 6;
+            for (int i = 0; i < level; i++)
+                _doc.Append("#");
+            _doc.Append(" ");
+            _doc.Append(content);
+            _doc.Append(Environment.NewLine);
+        }
+    }
+}
---
+++ MDTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDTools
+{
+    public class MDTable
+    {
+        public enum AlignMode
+        {
+            center,
+            left,
+            right
+        }
+        StringBuilder _table;
+        public MDTable()
+        {
+            _table = new StringBuilder(Environment.NewLine);
+        }
+        public void CreateTable(string[] titles, AlignMode alignMode)
+        {
+            if (titles.Length == 0) return;
+            for (int i = 0; i < titles.Length; i++)
+            {
+                _table.Append($"| {titles[i]}");
+            }
+            _table.Append("|\n");
+            string am = "";
+            switch (alignMode)
+            {
+                case AlignMode.center: am = ":-:"; break;
+                case AlignMode.left: am = "-:"; break;
+                case AlignMode.right: am = ":-"; break;
+            }
+            for (int i = 0; i < titles.Length; i++)
+            {
+                _table.Append($"|{am}");
+            }
+            _table.Append("|\n");
+        }
+        public void AppendRow(string[] row)
+        {
+            if (row.Length == 0) return;
+            for (int i = 0; i < row.Length; i++)
+            {
+                _table.Append($"|{row[i]}");
+            }
+            _table.Append("|\n");
+        }
+        public override string ToString()
+        {
+            var content = _table.ToString();
+            return string.IsNullOrEmpty(content) ? "" : _table.ToString();
+        }
+    }
+}
